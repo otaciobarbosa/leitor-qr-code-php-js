57de8d2efba0792812003eb9d85570a76521abe7
Otácio Barbosa <otacio.barbosa@outlook.com>
2023-05-08 22:32:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,6 @@
             document.addEventListener("DOMContentLoaded", fn);
         }
     }
-
     docReady(function () {
         var resultContainer = document.getElementById('qr-reader-results');
         var lastResult, countResults = 0;
@@ -27,7 +26,6 @@
                 console.log(`Scan result ${decodedText}`, decodedResult);
             }
         }
-
         var html5QrcodeScanner = new Html5QrcodeScanner(
             "qr-reader", { fps: 10, qrbox: 250 });
         html5QrcodeScanner.render(onScanSuccess);
